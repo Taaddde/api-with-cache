dbf956ab316d2649ad4d2c8c47a6bd40c08d2611
taaddde <taaddde@hotmail.com>
2023-05-15 16:46:29 -0300

    [FX] Menos funciones para un ejemplo mas claro

--- services/weather.service.js
+++ services/weather.service.js
@@ -12,51 +12,6 @@ const getCurrentByCity = async (city) => {
   }
 }
 
-const getCurrentByCoords = async (lat, lon) => {
-  const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.OPENWEATHER_KEY}&units=metric`
-
-  try {
-    const response = await fetch(url)
-    const data = await response.json()
-
-    return data
-  } catch (error) {
-    console.error(error)
-    return null
-  }
-}
-
-const getForecastByCity = async (city) => {
-  const url = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${process.env.OPENWEATHER_KEY}&units=metric`
-
-  try {
-    const response = await fetch(url)
-    const data = await response.json()
-
-    return data
-  } catch (error) {
-    console.error(error)
-    return null
-  }
-}
-
-const getForecastByCoords = async (lat, lon) => {
-  const url = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.OPENWEATHER_KEY}&units=metric`
-
-  try {
-    const response = await fetch(url)
-    const data = await response.json()
-
-    return data
-  } catch (error) {
-    console.error(error)
-    return null
-  }
-}
-
 module.exports = {
   getCurrentByCity,
-  getCurrentByCoords,
-  getForecastByCity,
-  getForecastByCoords
 }
